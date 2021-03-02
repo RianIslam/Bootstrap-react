e05871195f79d19a953380261de9ac183142281e
RianIslam <rianislamdr@gmail.com>
2021-03-03 01:58:54 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world
       </h1>
+      <Header/>
       <Button variant="danger">Danger</Button>
 
     </div>
